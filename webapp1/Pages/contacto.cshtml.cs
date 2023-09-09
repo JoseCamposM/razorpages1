29a03c4ef0dd90777eab3624f448a65a2dc9e38f
Jose Angel Campos Monsivais <55610980+JoseCamposM@users.noreply.github.com>
2023-09-09 14:36:13 -0600

    ARCHIVOS REALIZADOS EN LAPTOP

---
+++ webapp1/Pages/contacto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class contactoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
